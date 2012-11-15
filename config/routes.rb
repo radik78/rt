948519803c003a6183595e1986e3e39eec801ca6
radik <iq30@mail.ru>
2012-11-15 11:53:15 +0300

    created controller and model User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rt::Application.routes.draw do
+  get "users/index"
+
+  get "users/show"
+
+  get "users/new"
+
+  get "users/create"
+
+  get "users/edit"
+
+  get "users/update"
+
+  get "users/destroy"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
