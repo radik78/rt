6c8a99389e75b847a35bbc79e71f273b7d72fdfc
radik <iq30@mail.ru>
2012-11-19 16:58:40 +0300

    как то работает

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,6 +1,12 @@
 class PageController < ApplicationController
   def home
 	@user = User.new
+	respond_to do |format|
+		format.html
+		format.js {render :js=> "alert('hello');", :content_type=>'text/javascript'}
+		format.json {render :json=>@user}
+		format.xml  {render :xml=>@user}
+	end
   end
 
 end
