bf9df9fe95dd6dca0906a07f245153605507b766
radik <iq30@mail.ru>
2012-11-21 18:57:56 +0300

    изменил немного конотроллер сообщений

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,18 +1,24 @@
 class MessagesController < ApplicationController
   def index
-	@messages = Message.order('updated_at DESC').limit 2
-	#@messages = Message.all
+	   @messages = Message.order('updated_at DESC').limit 2
+	   #@messages = Message.all
   end
 
   def show
   end
 
   def new
-	@message = Message.new
+	 @message = Message.new
   end
 
   def create
+	 message = Message.new(params[:message])
+   if message.save
+    flash[:notice] = 'сообщение было добавлено'
+	  redirect_to messages_path
+
   end
+
 
   def edit
   end
